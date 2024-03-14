a57de7882a29419c8a2df81dccc6d24d64cbc397
Misha <mishaho@mail.com>
2024-03-14 09:30:57 +0100

    beter leesbaar

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -4,36 +4,30 @@ use \PHPUnit\Framework\TestCase;
 
 /* namespace VoorbeeldUnittest is gekoppeld aan de directory '/app' via composer.json
 "autoload":{
-        "psr-4":{
-            "VoorbeeldUnittest\\": "app"
-        }
+		"psr-4":{
+			"VoorbeeldUnittest\\": "app"
+		}
 */
 use \VoorbeeldUnittest\Calculator;
 
 // de classname moet gelijk zijn aan de filenaam CalculatorTest.php
-class CalculatorTest extends TestCase
-{
-    // Methods moeten starten met de naam test....
-    public function testAdd()
-     {
-        // Of zonder use: $calculator = new \VoorbeeldUnittest\Calculator; 
-        $calculator = new Calculator;
+class CalculatorTest extends TestCase {
 
-        // opdracht 1    
-        $this->assertEquals("abc...xyz", $calculator->nummerDrie("abcdefghijklmnopqrstuvwxyz"));
-        
-        
-        //opdracht 2 
-        $this->assertEquals("Te Klein", $calculator->nummerCheck("a"));
-        $this->assertEquals("abc...xyz", $calculator->nummerCheck("abcdefghijklmnopqrstuvwxyz"));
-        $this->assertEquals("Het moet een string zijn", $calculator->nummerCheck(false));
-        
+	// Methods moeten starten met de naam test....
+	public function testAdd() {
+		
+		// Of zonder use: $calculator = new \VoorbeeldUnittest\Calculator; 
+		$calculator = new Calculator;
 
-        // opdracht 3
-
-        $this->assertEquals("abc...xyz", $calculator->nummerDrie("abcdefghijklmnopqrstuvwxyz"));
-        
-
-    
-        }
-    }
+		// opdracht 1    
+		$this->assertEquals("abc...xyz", $calculator->nummerDrie("abcdefghijklmnopqrstuvwxyz"));
+		
+		//opdracht 2 
+		$this->assertEquals("Te Klein", $calculator->nummerCheck("a"));
+		$this->assertEquals("abc...xyz", $calculator->nummerCheck("abcdefghijklmnopqrstuvwxyz"));
+		$this->assertEquals("Het moet een string zijn", $calculator->nummerCheck(false));
+		
+		// opdracht 3
+		$this->assertEquals("abc...xyz", $calculator->nummerDrie("abcdefghijklmnopqrstuvwxyz"));
+	}
+}
